ebf4f060b752291d5ef6ae23991d02185b122469
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-10-31 09:20:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
-  //your code here
+   for(let i=0;i<arr.length;i++){
+	   arr[i]=arr[i]+1;
+   }
+	return arr;
 }
 
 alert(incrementArray(arr));
